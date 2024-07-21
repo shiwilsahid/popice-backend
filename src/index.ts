1db983866664e482619dda2d62fa8bc0fc3efc74
shiwilsahid <sieengelmein@gmail.com>
2024-07-21 11:57:25 +0700

    feat: API get products, product and setup cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,40 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { prisma } from "./libs/prisma";
 
 const app = new Hono();
 
+app.use("/*", cors());
+
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return c.json({
+    message: "Pop Ice Backend API",
+    products: "/api/products",
+  });
+});
+
+// PRODUCTS
+// Get Products
+app.get("/api/products", async (c) => {
+  const products = await prisma.product.findMany();
+
+  return c.json(products);
+});
+
+// | `/api/products/:slug` | `GET`    | Get Product by Slug    |
+app.get("/api/products/:slug", async (c) => {
+  const slug = c.req.param("slug");
+
+  const product = await prisma.product.findUnique({
+    where: { slug },
+  });
+
+  if (!product) {
+    c.status(404);
+    return c.json({ message: "Product is not found" });
+  }
+
+  return c.json(product);
 });
 
 export default app;
