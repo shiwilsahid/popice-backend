002d103ce4c1adbea3871f6b006bd8efc43fccf9
M Haidar Hanif <git@mhaidarhanif.com>
2024-07-29 21:19:45 +0700

    feat: add more products

--- prisma/data/products.ts
+++ prisma/data/products.ts
@@ -1,14 +1,47 @@
 export const dataProducts = [
-    {
-      slug: "pop-ice-variant-moccacino", // unique
-      name: "Pop Ice Variant - Moccacino", // unique
-      imageUrl:
-        "https://www.forisa.co.id/images/product/PopIceVariant_Coffee_Moccacino.png",
-    },
-    {
-      slug: "pop-ice-variant-lychee", // unique
-      name: "Pop Ice Variant - Lychee", // unique
-      imageUrl:
-        "https://www.forisa.co.id/images/product/PopIceVariant_Lychee.png",
-    },
-  ];
+  {
+    slug: "pop-ice-variant-moccacino", // unique
+    name: "Pop Ice Variant - Moccacino", // unique
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Coffee_Moccacino.png",
+  },
+  {
+    slug: "pop-ice-variant-lychee", // unique
+    name: "Pop Ice Variant - Lychee", // unique
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Lychee.png",
+  },
+  {
+    slug: "pop-ice-variant-mango", // unique
+    name: "Pop Ice Variant - Mango", // unique
+    imageUrl: "https://www.forisa.co.id/images/product/PopIceVariant_Mango.png",
+  },
+  {
+    slug: "pop-ice-variant-melon", // unique
+    name: "Pop Ice Variant - Melon", // unique
+    imageUrl: "https://www.forisa.co.id/images/product/PopIceVariant_Melon.png",
+  },
+  {
+    slug: "pop-ice-variant-chocolate", // unique
+    name: "Pop Ice Variant - Chocolate", // unique
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Chocolate_Chocolate.png",
+  },
+  {
+    slug: "pop-ice-variant-strawberry", // unique
+    name: "Pop Ice Variant - Strawberry", // unique
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Strawberry.png",
+  },
+  {
+    slug: "pop-ice-variant-avocado", // unique
+    name: "Pop Ice Variant - Avocado", // unique
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Avocado.png",
+  },
+  {
+    slug: "pop-ice-variant-guava", // unique
+    name: "Pop Ice Variant - Guava", // unique
+    imageUrl: "https://www.forisa.co.id/images/product/PopIceVariant_Guava.png",
+  },
+];
