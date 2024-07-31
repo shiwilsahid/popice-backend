158e2937775149092224400bd504261a261b9802
shiwilsahid <sieengelmein@gmail.com>
2024-08-01 01:07:54 +0700

    feat: more data products

--- prisma/data/products.ts
+++ prisma/data/products.ts
@@ -1,47 +1,183 @@
 export const dataProducts = [
   {
-    slug: "pop-ice-variant-moccacino", // unique
-    name: "Pop Ice Variant - Moccacino", // unique
+    slug: "pop-ice-variant-thunder-choco-malt",
+    name: "Pop Ice Variant - Thunder Choco Malt",
     imageUrl:
-      "https://www.forisa.co.id/images/product/PopIceVariant_Coffee_Moccacino.png",
+      "https://www.forisa.co.id/images/product/PopIceVariant_Active_Thunder%20ChocoMalt.png",
   },
   {
-    slug: "pop-ice-variant-lychee", // unique
-    name: "Pop Ice Variant - Lychee", // unique
+    slug: "pop-ice-variant-tornado-squash",
+    name: "Pop Ice Variant - Tornado Squash",
     imageUrl:
-      "https://www.forisa.co.id/images/product/PopIceVariant_Lychee.png",
+      "https://www.forisa.co.id/images/product/PopIceVariant_Active_TornadoSquash.png",
   },
   {
-    slug: "pop-ice-variant-mango", // unique
-    name: "Pop Ice Variant - Mango", // unique
-    imageUrl: "https://www.forisa.co.id/images/product/PopIceVariant_Mango.png",
-  },
-  {
-    slug: "pop-ice-variant-melon", // unique
-    name: "Pop Ice Variant - Melon", // unique
-    imageUrl: "https://www.forisa.co.id/images/product/PopIceVariant_Melon.png",
-  },
-  {
-    slug: "pop-ice-variant-chocolate", // unique
-    name: "Pop Ice Variant - Chocolate", // unique
+    slug: "pop-ice-variant-es-doger",
+    name: "Pop Ice Variant - Es Doger",
     imageUrl:
-      "https://www.forisa.co.id/images/product/PopIceVariant_Chocolate_Chocolate.png",
+      "https://www.forisa.co.id/images/product/PopIceVariant_Fusion_EsDoger.png",
   },
   {
-    slug: "pop-ice-variant-strawberry", // unique
-    name: "Pop Ice Variant - Strawberry", // unique
+    slug: "pop-ice-variant-es-teler",
+    name: "Pop Ice Variant - Es Teler",
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Fusion_EsTeler.png",
+  },
+  {
+    slug: "pop-ice-variant-yoghurt-strawberry",
+    name: "Pop Ice Variant - Yoghurt Strawberry",
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Fusion_YoghurtStrawberry.png",
+  },
+  {
+    slug: "pop-ice-variant-choco-cheese",
+    name: "Pop Ice Variant - Choco Cheese",
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Fusion_Choco_Cheese.png",
+  },
+  {
+    slug: "pop-ice-variant-cream-cheese",
+    name: "Pop Ice Variant - Cream Cheese",
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Fusion_CreamCheese.png",
+  },
+  {
+    slug: "pop-ice-variant-strawberry",
+    name: "Pop Ice Variant - Strawberry",
     imageUrl:
       "https://www.forisa.co.id/images/product/PopIceVariant_Strawberry.png",
   },
   {
-    slug: "pop-ice-variant-avocado", // unique
-    name: "Pop Ice Variant - Avocado", // unique
+    slug: "pop-ice-variant-taro",
+    name: "Pop Ice Variant - Taro",
+    imageUrl: "https://www.forisa.co.id/images/product/PopIceVariant_Taro.png",
+  },
+  {
+    slug: "pop-ice-variant-vanilla-blue",
+    name: "Pop Ice Variant - Vanilla Blue",
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_VanillaBlue.png",
+  },
+  {
+    slug: "pop-ice-variant-avocado",
+    name: "Pop Ice Variant - Avocado",
     imageUrl:
       "https://www.forisa.co.id/images/product/PopIceVariant_Avocado.png",
   },
   {
-    slug: "pop-ice-variant-guava", // unique
-    name: "Pop Ice Variant - Guava", // unique
+    slug: "pop-ice-variant-blueberry",
+    name: "Pop Ice Variant - Blueberry",
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Blueberry.png",
+  },
+  {
+    slug: "pop-ice-variant-grape",
+    name: "Pop Ice Variant - Grape",
+    imageUrl: "https://www.forisa.co.id/images/product/PopIceVariant_Grape.png",
+  },
+  {
+    slug: "pop-ice-variant-guava",
+    name: "Pop Ice Variant - Guava",
     imageUrl: "https://www.forisa.co.id/images/product/PopIceVariant_Guava.png",
+  },
+  {
+    slug: "pop-ice-variant-mung-bean",
+    name: "Pop Ice Variant - Mung Bean",
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_KacangHijau.png",
+  },
+  {
+    slug: "pop-ice-variant-lychee",
+    name: "Pop Ice Variant - Lychee",
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Lychee.png",
+  },
+  {
+    slug: "pop-ice-variant-mango",
+    name: "Pop Ice Variant - Mango",
+    imageUrl: "https://www.forisa.co.id/images/product/PopIceVariant_Mango.png",
+  },
+  {
+    slug: "pop-ice-variant-melon",
+    name: "Pop Ice Variant - Melon",
+    imageUrl: "https://www.forisa.co.id/images/product/PopIceVariant_Melon.png",
+  },
+  {
+    slug: "pop-ice-variant-bubble-gum",
+    name: "Pop Ice Variant - Bubble Gum",
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_PermenKaret.png",
+  },
+  {
+    slug: "pop-ice-variant-soursop",
+    name: "Pop Ice Variant - Soursop",
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Sirsak.png",
+  },
+  {
+    slug: "pop-ice-variant-moccacino",
+    name: "Pop Ice Variant - Moccacino",
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Coffee_Moccacino.png",
+  },
+  {
+    slug: "pop-ice-variant-vanilla-latte",
+    name: "Pop Ice Variant - Vanilla Latte",
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Coffee_VanillaLatte.png",
+  },
+  {
+    slug: "pop-ice-variant-white-koffee",
+    name: "Pop Ice Variant - White Koffee",
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Coffee_WhiteKoffee.png",
+  },
+  {
+    slug: "pop-ice-variant-choco-cookies",
+    name: "Pop Ice Variant - Choco Cookies",
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Chocolate_Choco_Cookies.png",
+  },
+  {
+    slug: "pop-ice-variant-choco-cream",
+    name: "Pop Ice Variant - Choco Cream",
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Chocolate_ChocoCream.png",
+  },
+  {
+    slug: "pop-ice-variant-chocolate",
+    name: "Pop Ice Variant - Chocolate",
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Chocolate_Chocolate.png",
+  },
+  {
+    slug: "pop-ice-variant-milk-chocolate",
+    name: "Pop Ice Variant - Milk Chocolate",
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Chocolate_CokelatSusu.png",
+  },
+  {
+    slug: "pop-ice-variant-avocado-cappuccino",
+    name: "Pop Ice Variant - Avocado Cappuccino",
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Coffee_AvocadoCappuccino.png",
+  },
+  {
+    slug: "pop-ice-variant-cappuccino",
+    name: "Pop Ice Variant - Cappuccino",
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Coffee_Cappuccino.png",
+  },
+  {
+    slug: "pop-ice-variant-coffee-latte",
+    name: "Pop Ice Variant - Coffee Latte",
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Coffee_CoffeeLatte.png",
+  },
+  {
+    slug: "pop-ice-variant-durian-cappuccino",
+    name: "Pop Ice Variant - Durian Cappuccino",
+    imageUrl:
+      "https://www.forisa.co.id/images/product/PopIceVariant_Coffee_DurianCappuccino.png",
   },
 ];
